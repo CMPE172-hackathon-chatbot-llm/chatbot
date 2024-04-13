030b02e0b7c93114a547e569899cfc731a948a6c
gcrabtree124 <103467504+gcrabtree124@users.noreply.github.com>
2024-04-13 15:26:35 -0700

    multiple prompts shown, llm in progress

--- app.py
+++ app.py
@@ -9,8 +9,31 @@ import streamlit as st
 # Bring in watson interface
 #from watsonxlangchain import LangChainInterface
 
+#create llm thru langchain
+creds = {
+    'apikey' : 'cpd-apikey-IBMid-691000CM03-2024-04-13T21:31:37Z',
+    'url':'https://us-south.ml.cloud.ibm.com'
+}
+llm = LangChainInterface(
+    credentials = creds,
+    model = 'meta-llama/llama-2-70b-chat',
+    params = {
+        'decoding_method':'sample',
+        'max_new_tokens':200,
+        'temperature':0.5
+    },
+    project_id='baa5d59f-e936-4dfa-9db1-6fac7fbef880')
+
+
 # setup the app title
 st.title('State of Food Security and Nutrition in the World 2023')
+
+#show prev prompts
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    st.chat_message(message['role']).markdown(message['content'])
 
 #Build a prompt input template to display the prompts
 prompt = st.chat_input('Pass Your Prompt Here')
@@ -19,3 +42,5 @@ prompt = st.chat_input('Pass Your Prompt Here')
 if prompt:
     # Display the prompt
     st.chat_message('user').markdown(prompt)
+    #for prev prompts
+    st.session_state.messages.append({'role':'user', 'content':prompt})
--- watsonxlangchain.py
+++ watsonxlangchain.py
@@ -5,12 +5,14 @@ from typing import Any, List, Mapping, Optional, Union, Dict
 from pydantic import BaseModel, Extra
 from ibm_watson_machine_learning.foundation_models import Model
 
+class CommonMeta(type(LLM), type(BaseModel)):
+    pass
 
-class LangChainInterface(LLM, BaseModel):
+class LangChainInterface(LLM, BaseModel, metaclass=CommonMeta):
     credentials: Optional[Dict] = None
     model: Optional[str] = None
     params: Optional[Dict] = None
-    project_id : Optional[str]=None
+    project_id: Optional[str] = None
 
     class Config:
         """Configuration for this pydantic object."""
