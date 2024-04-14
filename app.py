188b3c84d3af4be7db70fd0585e282f45b4e0a48
Josh Ebreo <ebreojoshua@gmail.com>
2024-04-13 18:12:24 -0700

    Removed unnecessary files and updated app.py

--- app.py
+++ app.py
@@ -4,62 +4,56 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import  RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 # Bring in Streamlit for UI dev
 import streamlit as st
-# Bring in watson interface
-from watsonxlangchain import LangChainInterface
 
-# Setup credentials dictionary
-cred = {
-    'apikey':'cpd-apikey-IBMid-691000CM03-2024-04-13T21:31:37Z',
-    'url':'https://us-south.ml.cloud.ibm.com'
+# Niti's API key: EzjmGauMG1UYYzBu27xpIZR6EsLIx-uGOvAcEc7qOspW
+import os
+from getpass import getpass
+
+# Prompt the user for the API key
+watsonx_api_key = 'EzjmGauMG1UYYzBu27xpIZR6EsLIx-uGOvAcEc7qOspW'
+
+# Set the API key as an environment variable
+os.environ["WATSONX_APIKEY"] = watsonx_api_key
+
+parameters = {
+    "decoding_method": "sample",
+    "max_new_tokens": 999,
+    "min_new_tokens": 1,
+    "temperature": 0.5,
+    "top_k": 50,
+    "top_p": 1,
 }
-# Create LLM using Langchain
-llm = LangChainInterface(
-    credentials = cred,
-    model = 'meta-llama/llama-2-70b-chat',
-    params = {
-        'decoding_method':'sample',
-        'max_new_tokens':200,
-        'temperature':0.5
-    },
-    project_id='baa5d59f-e936-4dfa-9db1-6fac7fbef880')
 
-# This function laods a Food PDF
-def load_pdf():
-    # Update the PDF
-    pdf = 'SOFI-2023.pdf'
-    loaders = [PyPDFLoader(pdf)]
-    # Create index - aka vector database - ask chromadb
-    index = VectorstoreIndexCreator(
-        embedding = HuggingFaceEmbeddings(model_name='all-MiniLM-L12-v2'),
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    ).from_loaders(loaders)
-    # Return the vector database
-    return index
-# Load the PDF
-index = load_pdf()
+# Bring in watson interface
+#from watsonxlangchain import LangChainInterface
+from langchain_ibm import WatsonxLLM
 
-# Create a Q&A chain
-chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type='stuff',
-    retriever=index.vectorstore.as_retriever(),
-    input_key='question'
+watsonx_llm = WatsonxLLM(
+    model_id="meta-llama/llama-2-70b-chat",
+    url="https://us-south.ml.cloud.ibm.com",
+    project_id="e49cf2c9-0f52-481a-af0f-f310e2f2eb35",
+    params=parameters,
 )
 
-# Setup the app title
-st.title('Ask Food Bot')
+# setup the app title
+st.title('Ask F.O.O.D. Bot Anything!')
 
-# Setup a session state message variable to hold all the old messages
+# Adding a subheader as subtitle
+st.subheader('The F.O.O.D. Bot is here to help you with your food-related questions!')
+
+# Setup a session state message variable  to hold all old messages
 if 'messages' not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages= []
 
-# Display message history
+# Display all historical messages
 for message in st.session_state.messages:
-    st.chat_message(message['role']).markdown(message['content']) 
+    st.chat_message(message['role']).markdown(message['content'])
 
-# Build a prompt input template to display the prompts
+
+#Build a prompt input template to display the prompts
 prompt = st.chat_input('Pass Your Prompt Here')
 
 # If the user hits enter then
@@ -68,11 +62,11 @@ if prompt:
     st.chat_message('user').markdown(prompt)
     # Store the user prompt in state
     st.session_state.messages.append({'role':'user', 'content':prompt})
-    # Send the prompt to the PDF Q&A CHAIN
-    response = chain.run(prompt)
-    # Show the LLM response
+    # send the prompt to llm
+    response = watsonx_llm(prompt)
+    # show llm response
     st.chat_message('assistant').markdown(response)
-    # Store the LLM response in state
+    # store llm response in state
     st.session_state.messages.append(
         {'role':'assistant', 'content':response}
     )
